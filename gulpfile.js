84e01640373ab1b74146e7acd9abd79e76797bd9
Pavel Rapoport <pavel.rapoport@gmail.com>
2014-11-20 08:39:22 +0200

    API fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 /******************
  * Gulp Dependencies
-******************/
+ ******************/
 
-var gulp = require('gulp'); 						// Gulp
-var server = require('gulp-develop-server');		// Run Node Server
-var connect = require('gulp-connect'); 				// Live Reload & Watch index.html
-var less = require('gulp-less'); 					// Less
-var autoprefixer = require('gulp-autoprefixer'); 	// Scc Prefixer
-var plumber = require('gulp-plumber');				// Like Compass
-var uglify = require('gulp-uglify');				// Uglify Js File
-var concat = require('gulp-concat');				// Concat Files
+var gulp = require('gulp'); // Gulp
+var server = require('gulp-develop-server'); // Run Node Server
+var connect = require('gulp-connect'); // Live Reload & Watch index.html
+var less = require('gulp-less'); // Less
+var autoprefixer = require('gulp-autoprefixer'); // Scc Prefixer
+var plumber = require('gulp-plumber'); // Like Compass
+var uglify = require('gulp-uglify'); // Uglify Js File
+var concat = require('gulp-concat'); // Concat Files
 
 
 
@@ -22,22 +22,22 @@ var concat = require('gulp-concat');				// Concat Files
 var appName = 'mongooseGenericApi';
 
 var versions = {
-	app: 'v0.0.1',
-	script: 'v0.0.1',
-	style: 'v0.0.1'
+    app: 'v0.0.1',
+    script: 'v0.0.1',
+    style: 'v0.0.1'
 };
 
-var paths = new function(){
-	this.app = ['./app/'];
-	this.server = ['./server.js'];
-	this.scripts = [this.app + 'scripts/**/*.js'];
-	this.vendors = ['./bower_components/'];
-	this.styles  = [this.app + 'styles/**/*.less'];
-	this.html = [this.app + '**/*.html'];
-	this.dest = {
-		script: this.app + 'dest',
-		style: this.app + 'dest'
-	};
+var paths = new function() {
+    this.app = ['./app/'];
+    this.server = ['./server.js'];
+    this.scripts = [this.app + 'scripts/**/*.js'];
+    this.vendors = ['./bower_components/'];
+    this.styles = [this.app + 'styles/**/*.less'];
+    this.html = [this.app + '**/*.html'];
+    this.dest = {
+        script: this.app + 'dest',
+        style: this.app + 'dest'
+    };
 };
 
 
@@ -47,11 +47,11 @@ var paths = new function(){
  * Connect
  ******************/
 
-gulp.task('connect',function(){
-	connect.server({
-		root: paths.app,
-		livereload: true
-	});
+gulp.task('connect', function() {
+    connect.server({
+        root: paths.app,
+        livereload: true
+    });
 });
 
 
@@ -60,13 +60,13 @@ gulp.task('connect',function(){
  * Scripts ( JS )
  ******************/
 
-gulp.task('scripts',function(){
-	gulp.src(paths.scripts)
-			.pipe(plumber())
-			.pipe(uglify())
-			.pipe(concat('min.'+appName+'.'+versions.script+'.js'))
-			.pipe(gulp.dest(paths.dest.script))
-			.pipe(connect.reload());
+gulp.task('scripts', function() {
+    gulp.src(paths.scripts)
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(concat('min.' + appName + '.' + versions.script + '.js'))
+        .pipe(gulp.dest(paths.dest.script))
+        .pipe(connect.reload());
 });
 
 
@@ -74,36 +74,36 @@ gulp.task('scripts',function(){
  * Concat Bower-Components ( JS )
  ******************/
 
-gulp.task('vendors',function(){
-	gulp.src([paths.vendors+'angular/angular.js'])
-			.pipe(plumber())
-			.pipe(concat('min.vendors.js'))
-			.pipe(gulp.dest(paths.dest.script))
-			.pipe(connect.reload());
+gulp.task('vendors', function() {
+    gulp.src([paths.vendors + 'angular/angular.js'])
+        .pipe(plumber())
+        .pipe(concat('min.vendors.js'))
+        .pipe(gulp.dest(paths.dest.script))
+        .pipe(connect.reload());
 });
 
 
 /******************
  * Styles ( LESS )
  ******************/
-gulp.task('styles',function(){
-	gulp.src(paths.styles)
-			.pipe(plumber())
-			.pipe(less())
-			.pipe(autoprefixer({
-				browsers: ['last 2 versions'],
-				cascade: false
-			}))
-			.pipe(gulp.dest(paths.dest.style))
-			.pipe(connect.reload());
+gulp.task('styles', function() {
+    gulp.src(paths.styles)
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(paths.dest.style))
+        .pipe(connect.reload());
 });
 
 /******************
  * Html
  ******************/
-gulp.task('html',function(){
-	gulp.src(paths.html)
-			.pipe(connect.reload());
+gulp.task('html', function() {
+    gulp.src(paths.html)
+        .pipe(connect.reload());
 });
 
 
@@ -113,14 +113,14 @@ gulp.task('html',function(){
 /******************
  * Node Server
  ******************/
-gulp.task('server:start',function(){
-	server.listen({
-		path: paths.server
-	});
+gulp.task('server:start', function() {
+    server.listen({
+        path: paths.server
+    });
 });
 
-gulp.task('server:restart',function(){
-	gulp.watch([paths.server],server.restart());
+gulp.task('server:restart', function() {
+    gulp.watch([paths.server], server.restart());
 });
 
 
@@ -128,11 +128,11 @@ gulp.task('server:restart',function(){
 /******************
  * Gulp Watch
  ******************/
-gulp.task('watch',function(){
-	gulp.watch([paths.scripts],['scripts']);
-	gulp.watch([paths.vendors],['vendors']);
-	gulp.watch([paths.styles],['styles']);
-	gulp.watch([paths.html],['html']);
+gulp.task('watch', function() {
+    gulp.watch([paths.scripts], ['scripts']);
+    gulp.watch([paths.vendors], ['vendors']);
+    gulp.watch([paths.styles], ['styles']);
+    gulp.watch([paths.html], ['html']);
 
 });
 
@@ -140,4 +140,4 @@ gulp.task('watch',function(){
 /******************
  * Gulp Default
  ******************/
-gulp.task('default',['connect','watch']);
+gulp.task('default', ['connect', 'watch']);
--- test/test.js
+++ test/test.js
@@ -1,21 +1,15 @@
 var assert = require("assert");
+var express = require('express');
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var express = require('express');
-var app = express();
 var request = require('request');
 var bodyParser = require('body-parser');
 
 
-
-var testModelId = '';
-var testModelObject;
-
-
-
-// returning data model for test
-// -----------------------------------
-var getTestModelObject = function() {
+/******************
+ * Test Object
+ ******************/
+var getTestObject = function() {
     return {
         date: new Date(),
         name: 'name',
@@ -23,11 +17,13 @@ var getTestModelObject = function() {
     }
 };
 
+
 before(function(done) {
 
 
-    // 
-    // -----------------------------------
+    /******************
+     * Creating test schema and testing object
+     ******************/
     var testSchema = new Schema({
         date: {
             type: Date,
@@ -37,16 +33,23 @@ before(function(done) {
         number: Number
     });
 
-    testModelObject = getTestModelObject();
-
+    // Adding schema to mongoose
     testModel = mongoose.model('Test', testSchema);
 
-    var apiRouter = require('./../src/index');
+    // Getting test object
+    testObject = getTestObject();
 
+    // Creating express application
+    var app = express();
+
+    // Body (JSON) parsing middleware
     app.use(bodyParser.json());
-    app.use('/api/', apiRouter);
 
+    app.use('/api/', require('./../lib/index'));
 
+    /******************
+     * Connecting to mongo and starting server
+     ******************/
     mongoose.connect("mongodb://localhost:27017/test", function() {
 
         console.log('Mongo connected');
@@ -55,7 +58,12 @@ before(function(done) {
 
             console.log('Server started');
 
-            done();
+            // Removing data from test table
+            mongoose.connection.collections['tests'].drop(function(err) {
+                
+                done();
+
+            });
 
         });
 
@@ -64,27 +72,29 @@ before(function(done) {
 });
 
 
+var url = 'http://localhost:3000/api/tests';
+var testObject = {};
+var testModelId = '';
+
+
 describe('Testing API', function() {
 
     it('Creating model', function(done) {
 
-
         request.post({
-            url: 'http://localhost:3000/api/tests',
-            json: testModelObject
+            url: url,
+            json: testObject
         }, function(err, httpResponse, body) {
 
             assert.equal(err, null);
             assert.equal(httpResponse.statusCode, 200);
-            assert.equal(body.name, testModelObject.name);
-            assert.equal(body.number, testModelObject.number);
+            assert.equal(body.name, testObject.name);
+            assert.equal(body.number, testObject.number);
 
             testModelId = body._id;
 
-
-            // TODO
-            // check assert of the date 
-            // assert.equal(body.date, testModelObject.date);
+            // TODO: check assert of the date 
+            // assert.equal(body.date, testObject.date);
 
             done();
 
@@ -92,18 +102,21 @@ describe('Testing API', function() {
 
     });
 
+
     it('Getting single model', function(done) {
 
         request.get({
-            uri: 'http://localhost:3000/api/tests/' + testModelId
+
+            uri: url + '/' + testModelId
+
         }, function(err, httpResponse, body) {
 
             body = JSON.parse(body);
 
             assert.equal(err, null);
             assert.equal(httpResponse.statusCode, 200);
-            assert.equal(body.name, testModelObject.name);
-            assert.equal(body.number, testModelObject.number);
+            assert.equal(body.name, testObject.name);
+            assert.equal(body.number, testObject.number);
 
             done();
 
@@ -111,28 +124,33 @@ describe('Testing API', function() {
 
     });
 
+
     it('Updating model', function(done) {
 
-        testModelObject.name = 'name-update';
-        testModelObject.number = 19;
+        testObject.name = 'name-update';
+        testObject.number = 19;
 
         request.put({
-            uri: 'http://localhost:3000/api/tests/' + testModelId,
-            json: testModelObject
+
+            uri: url + '/' + testModelId,
+            json: testObject
+
         }, function(err, httpResponse, body) {
 
             assert.equal(httpResponse.statusCode, 200);
 
             request.get({
-                uri: 'http://localhost:3000/api/tests/' + testModelId
+
+                uri: url + '/' + testModelId
+
             }, function(err, httpResponse, body) {
 
                 body = JSON.parse(body);
 
                 assert.equal(err, null);
                 assert.equal(httpResponse.statusCode, 200);
-                assert.equal(body.name, testModelObject.name);
-                assert.equal(body.number, testModelObject.number);
+                assert.equal(body.name, testObject.name);
+                assert.equal(body.number, testObject.number);
 
                 done();
 
